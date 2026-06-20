3b3cbca168ade757b134570cf6e84b74ac265791
Richard Shade <rshade98@hotmail.com>
2026-06-19 19:13:00 -0500

    docs: update sitemap

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -133,5 +134,6 @@ export default defineConfig({
         },
       ],
     }),
+    sitemap(),
   ],
 });
